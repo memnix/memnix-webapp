ea1d6f8b9033234eac77c414ad1f40d8b3d0578f
CorentinGS <c.giaufersaubert@outlook.com>
2022-05-08 10:50:16 +0200

    :tada: Nuxt3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,28 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from "nuxt";
 
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-})
+    modules: [
+        '@nuxtjs/tailwindcss'
+    ],
+    buildModules: [
+        // pinia plugin - https://pinia.esm.dev
+        "@pinia/nuxt",
+    ],
+    build: {
+        transpile: ['@heroicons/vue'],
+    },
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
+    vite: {
+        logLevel: "info",
+        optimizeDeps: {
+            include: [
+                '@headlessui/vue', '@heroicons/vue/solid', '@heroicons/vue/outline', 'vue', 'pinia', 'ufo'
+            ]
+        }
+    }
+});
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+    content: [
+        "./components/**/*.{js,vue,ts}",
+        "./layouts/**/*.vue",
+        "./pages/**/*.vue",
+        "./plugins/**/*.{js,ts}",
+        "./app.vue",
+        "./assets/**/*.scss",
+        "./assets/**/*.css",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['"Inter var"', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [
+        require("@tailwindcss/forms"),
+        require("@tailwindcss/typography"),
+        require("@tailwindcss/line-clamp"),
+        require("@tailwindcss/aspect-ratio"),
+    ],
+};
