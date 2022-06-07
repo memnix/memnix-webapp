d25428487f8075db7d4288282b3e9e9de49a11e7
CorentinGS <c.giaufersaubert@outlook.com>
2022-06-07 15:30:59 +0200

    :globe_with_meridians: Add i18n support

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <Html :lang="locale">
+  <Body
+    class=" duration-300 transition-colors text-gray-800 dark:text-gray-200 bg-white dark:bg-gray-900"
+  >
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+  </Body>
+  </Html>
+</template>
+
+<script lang="ts" setup>
+import { LanguageManager } from '~/utils/lang'
+const languageManager = LanguageManager()
+const locale = useState<string>('locale.setting')
+</script>
