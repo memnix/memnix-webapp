d1ad84565a3b369f518ec08cf75d2c7dfbafe996
CorentinGS <c.giaufersaubert@outlook.com>
2022-06-22 13:13:45 +0200

    ✨ Play page

---
+++ components/Core/Card.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="text-center">
+    <h3 class="my-3 text-xl md:text-2xl lg:text-3xl">
+      Quelle est la plus ancienne distribution Linux ?
+    </h3>
+    <div class="flex justify-center max-x-2xl ">
+        <img
+          src="https://api.lorem.space/image/movie?w=512&h=512&hash=8B7BCDC2"
+          class='object-cover object-center rounded-box shadow-lg object-fill'
+          loading="lazy"
+          width="100%"
+          height="100%"
+        />
+
+    </div>
+
+    <div class="grid max-w-2xl grid-cols-2 grid-rows-2 gap-4 pt-10 pb-5">
+      <div class="hoveranimation btn btn-primary w-full">One</div>
+      <div class="hoveranimation btn btn-primary w-full">One</div>
+      <div class="hoveranimation btn btn-primary w-full">One</div>
+      <div class="hoveranimation btn btn-primary w-full">One</div>
+    </div>
+  </div>
+</template>
+
+<script setup lang='ts'>
+
+</script>
+
+<style scoped>
+
+</style>
--- pages/play.vue
+++ pages/play.vue
@@ -1,27 +1,12 @@
-<template >
-  <HeaderPlay />
-  <div
-    class="md:pt-15 flex flex-col items-center px-5 pt-2 lg:px-20 lg:pt-10"
-  >
-    <div class="text-center">
-      <h3 class="my-5 text-xl md:text-2xl lg:text-3xl">
-        Quelle est la plus ancienne distribution Linux ?
-      </h3>
-      <div class="rounded-box max-w-2xl shadow-lg">
-        <img
-          src="https://api.lorem.space/image/movie?w=1024&h=1024&hash=8B7BCDC2"
-          loading="lazy"
-          width="1024px"
-          height="1024px"
-        />
-      </div>
-
-      <div class="grid max-w-2xl grid-cols-2 grid-rows-2 gap-4 py-10">
-        <div class="hoveranimation btn btn-primary w-full">One</div>
-        <div class="hoveranimation btn btn-primary w-full">One</div>
-        <div class="hoveranimation btn btn-primary w-full">One</div>
-        <div class="hoveranimation btn btn-primary w-full">One</div>
-      </div>
+<template>
+  <div class="max-h-screen">
+    <div class="flex flex-col items-center px-5 pt-2 md:pt-5 lg:px-20">
+      <CoreCard />
+      <progress
+        class="progress progress-primary max-w-xl"
+        value="40"
+        max="100"
+      ></progress>
     </div>
   </div>
 </template>
@@ -29,9 +14,4 @@
 <script setup lang="ts"></script>
 
 <style scoped></style>
-<script>
-import HeaderPlay from '../components/Utils/HeaderPlay'
-export default {
-  components: { HeaderPlay },
-}
-</script>
+<script></script>
