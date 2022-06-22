d9ff754e2fdec44398ffeb7634b02123bf5c5acb
CorentinGS <c.giaufersaubert@outlook.com>
2022-06-22 13:21:48 +0200

    ✨ Back button

--- pages/play.vue
+++ pages/play.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="max-h-screen">
     <div class="flex flex-col items-center px-5 pt-2 md:pt-5 lg:px-20">
+      <div class="absolute left-0 top-[0.5rem] hoveranimation">
+        <NuxtLink class="btn btn-ghost text-xl md:text-2xl lg:text-3xl" to="/home">
+          <Icon-lucide-arrow-left  />
+        </NuxtLink>
+      </div>
       <CoreCard />
       <progress
         class="progress progress-primary max-w-xl"
