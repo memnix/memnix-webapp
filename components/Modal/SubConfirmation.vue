a3b4f2b8fad923666b0f891912305653346a94e8
CorentinGS <c.giaufersaubert@outlook.com>
2022-07-06 15:01:11 +0200

    💄 Modal border

--- components/Modal/SubConfirmation.vue
+++ components/Modal/SubConfirmation.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="modal modal-open modal-bottom sm:modal-middle ">
-    <div class="modal-box border-4 border-secondary">
+    <div class="modal-box border-2 border-secondary">
       <h3 class="text-lg md:text-xl lg:text-2xl font-bold">Subscribe to this deck ?</h3>
       <p class="py-4 text-md md:text-lg lg:text-xl">You will be able to play it</p>
       <div class="modal-action">
