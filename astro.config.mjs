014d7a860dc67bc5aa75f387e7ff6f3b38f98b8b
CorentinGS <c.giaufersaubert@outlook.com>
2023-02-26 11:09:18 +0100

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -81,9 +81,6 @@ export default defineConfig({
 		]
 	},
 	output: "server",
-	//adapter: vercel()
-	adapter: node({
-			mode: "standalone",
-		}
-	)
+	adapter: vercel()
+	// adapter: node({mode: "standalone"})
 })
