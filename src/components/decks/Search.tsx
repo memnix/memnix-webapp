08e1e8fb2baeeb7a759eb8a27af2f5695e1d4d3e
CorentinGS <c.giaufersaubert@outlook.com>
2023-03-04 14:54:54 +0100

    i18n

--- src/components/decks/Search.tsx
+++ src/components/decks/Search.tsx
@@ -7,17 +7,17 @@ import {
 } from "react-instantsearch-dom"
 import { instantMeiliSearch } from "@meilisearch/instant-meilisearch"
 
-export default function Counter({ children, count }) {
+export default function Search({ meilisearchToken }) {
 	const searchClient = instantMeiliSearch(
-		"https://ms-adf78ae33284-106.lon.meilisearch.io",
-		"a63da4928426f12639e19d62886f621130f3fa9ff3c7534c5d179f0f51c4f303"
+		"http://localhost:7700",
+		meilisearchToken
 	)
 
 	const Hit = ({ hit }) => <Highlight attribute="name" hit={hit} />
 
 	return (
 		<>
-			<InstantSearch indexName="steam-video-games" searchClient={searchClient}>
+			<InstantSearch indexName="decks" searchClient={searchClient}>
 				<SearchBox />
 				<Hits hitComponent={Hit} />
 			</InstantSearch>
