a374b69a22ef6bfe52cb004e285012450b19263b
CorentinGS <c.giaufersaubert@outlook.com>
2023-09-11 20:45:13 +0200

    :fire: Nuxt

--- app.vue
+++ app.vue
@@ -3,3 +3,19 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script lang="ts" setup>
+  useHead({
+    script: [
+      {
+        src: "https://umami.memnix.app/script.js",
+        "data-website-id": "ba949ed3-9c0b-4c8c-901a-33eebcb67d49",
+        async: true,
+      },
+      {
+        src: "/js/gtag.js",
+        async: true,
+      },
+    ],
+  })
+</script>
